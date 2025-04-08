de437ffd388b91435df9989c33b65f37152f78c8
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-04-08 18:36:44 +0200

    add indexes  for the migrations

--- database/migrations/2025_04_07_214813_create_posts_table.php
+++ database/migrations/2025_04_07_214813_create_posts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('content', 1000); 
+            $table->string('title')->index();
+            $table->string('content', 1000)->index(); 
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
             $table->softDeletes();
