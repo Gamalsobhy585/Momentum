3694e26f7e71e3a6d67fdacd94f699fea0a89574
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-04-08 23:41:38 +0200

    update cache on change posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Cache;
+
 
 class Post extends Model
 {
@@ -21,4 +23,25 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+
+    protected static function booted()
+{
+    static::created(function ($post) {
+        Cache::forget('user:info:' . $post->user_id);
+    });
+    
+    static::updated(function ($post) {
+        Cache::forget('user:info:' . $post->user_id);
+    });
+    
+    static::deleted(function ($post) {
+        Cache::forget('user:info:' . $post->user_id);
+    });
+    
+    static::restored(function ($post) {
+        Cache::forget('user:info:' . $post->user_id);
+    });
+}
+
 }
